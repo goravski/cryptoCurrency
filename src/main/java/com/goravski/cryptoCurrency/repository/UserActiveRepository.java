2cba497bf185e5fefd1526cf450d83df5f0b786e
Evgen Goravski <evgoravsky@gmail.com>
2023-05-17 19:54:30 +0300

    feat:  method get UserActive by crypto symbol from database

--- src/main/java/com/goravski/cryptoCurrency/repository/UserActiveRepository.java
+++ src/main/java/com/goravski/cryptoCurrency/repository/UserActiveRepository.java
@@ -5,8 +5,12 @@ import com.goravski.cryptoCurrency.model.UserActive;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserActiveRepository extends JpaRepository <UserActive, Long> {
 
 
+   Optional <List<UserActive>> findAllBySymbol(String symbol);
 }
